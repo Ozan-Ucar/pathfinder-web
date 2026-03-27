1f64d29af096502c27e9428828f636302c96e823
ou1534s <ozan-ucar@outlook.de>
2026-03-27 15:10:00 +0200

    wall drawing with mouse

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const ROWS = 20;
 const COLS = 40;
 const grid = document.getElementById('grid-container');
 
+let isDrawing = false;
+
 function initGrid() {
     grid.style.gridTemplateColumns = `repeat(${COLS}, 25px)`;
     for(let r=0; r<ROWS; r++) {
@@ -9,9 +11,21 @@ function initGrid() {
             const node = document.createElement('div');
             node.className = 'node';
             node.id = `node-${r}-${c}`;
+            
+            node.addEventListener('mousedown', () => {
+                isDrawing = true;
+                node.classList.toggle('wall');
+            });
+            node.addEventListener('mouseenter', () => {
+                if(isDrawing) node.classList.toggle('wall');
+            });
+            node.addEventListener('mouseup', () => isDrawing = false);
+            
             grid.appendChild(node);
         }
     }
 }
+
+grid.addEventListener('mouseleave', () => isDrawing = false);
 
 initGrid();
