53928a7e60cc0667c4a2ad4ad0fbdfba237bed84
ou1534s <ozan-ucar@outlook.de>
2026-03-27 16:05:00 +0200

    start and end markers

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const COLS = 40;
 const grid = document.getElementById('grid-container');
 
 let isDrawing = false;
+let startNode = {r: 10, c: 5};
+let endNode = {r: 10, c: 35};
 
 function initGrid() {
     grid.style.gridTemplateColumns = `repeat(${COLS}, 25px)`;
@@ -12,18 +14,26 @@ function initGrid() {
             node.className = 'node';
             node.id = `node-${r}-${c}`;
             
+            if(r === startNode.r && c === startNode.c) node.classList.add('start');
+            if(r === endNode.r && c === endNode.c) node.classList.add('end');
+            
             node.addEventListener('mousedown', () => {
                 isDrawing = true;
-                node.classList.toggle('wall');
+                toggleWall(node, r, c);
             });
             node.addEventListener('mouseenter', () => {
-                if(isDrawing) node.classList.toggle('wall');
+                if(isDrawing) toggleWall(node, r, c);
             });
             node.addEventListener('mouseup', () => isDrawing = false);
             
             grid.appendChild(node);
         }
     }
+}
+
+function toggleWall(node, r, c) {
+    if((r===startNode.r && c===startNode.c) || (r===endNode.r && c===endNode.c)) return;
+    node.classList.toggle('wall');
 }
 
 grid.addEventListener('mouseleave', () => isDrawing = false);
